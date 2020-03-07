3f5e6c52555a0efd152d04bcf9954bc1efaf2267
debshila <debshila@gmail.com>
2020-03-07 15:52:17 -0600

    Changed the dimension of the plots.

--- covid_visualizations.R
+++ covid_visualizations.R
@@ -44,6 +44,6 @@ world_plot <- world_map +
     ggtitle('Date {closest_state}') +
     labs(x= '', y='', color = "Outcomes", size = "Cumulative number of cases") +
     theme(axis.text = element_blank(), axis.ticks = element_blank())
-animate(world_plot, height = 1000, width =1500)
+animate(world_plot, height = 300, width = 500)
 
 anim_save("coronavirus_spread.gif", world_plot)
